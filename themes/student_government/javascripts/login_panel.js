507f19b7015e69fdfdbd1217a6a30923069b46e3
Oliver Spryn <git@oliver.tech>
2012-04-02 21:46:21 +0000

    The student_government theme underwent the following changes in this revision: (1) the Question of the Week was removed due to the lack of space, (2) the "LOGIN/SIGN UP" button was reworded to "BOOK EXCHANGE LOGIN" for clarity, (3) the "LOGOUT" button was stretched to the same size as the login button, (4) breadcrumb capabilities and accompanying images were added to the CSS, (5) sub-link navigation capabilities were added to the CSS, (6) links in the header and footer can now indicate whether the page requires a login, and displays a lock icon if that is the case, and (7) the stylesheet now enforces better HTML5 semantics.

--- themes/student_government/javascripts/login_panel.js
+++ themes/student_government/javascripts/login_panel.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-//Slide down the login and sign-up panel
-	$('img.tag').click(function() {
+//Slide down the login and sign-up panel when the login flag is clicked
+	$('section.flag img').click(function() {
 		$('section.login').slideToggle();
+	});
+	
+//Slide down the login and sign-up panel when a locked link is clicked
+	$('a.locked').click(function(event) {
+	//Don't follow the page!
+		event.preventDefault();
+		
+	//Slide down the panel
+		$('section.login').slideDown();
+		
+	//Slide to the top of the page, where the panel resides
+		$('html, body').animate({
+			scrollTop: 0
+		}, 'slow');
+		
+	//Set the hidden redirect parameter for the login form
+		$('#redirect').val($(this).attr('href'));
 	});
 });
